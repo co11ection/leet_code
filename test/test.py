ccb5678b7098822c972cee5766a3d14a9bd00c86
tima <tima.j.zh@gmail.com>
2023-08-16 12:44:47 +0600

    integer to roman

--- test/test.py
+++ test/test.py
@@ -1,6 +1,19 @@
-def calculate_power(base, exponent):
-    return base ** exponent
+num = 10342
+rom_number = {
+            1: "I", 
+            5: "V", 4: "IV",
+            10: "X", 9: "IX",
+            50: "L", 40: "IL",
+            100: "C", 90: "XC",
+            500: "D", 400: "CD",
+            1000: "M", 900: "CM"
+        }
+result = ""
+ls = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
-
-result = calculate_power(2, 3)
+print(ls)
+for n in ls:
+    while n<=num:
+        result += rom_number[n]
+        num -= n
 print(result)
