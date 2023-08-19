00080da851db2e7c4c4bfe6b93b6e0ab0abd8c39
tima <tima.j.zh@gmail.com>
2023-08-19 21:02:59 +0600

    spiral matrix II

---
+++ medium/59.py
@@ -0,0 +1,27 @@
+class Solution:
+    def generateMatrix(self, n: int) -> list[list[int]]:
+        matrix = [[0] * n for i in range(n)]
+        num = 1
+        left, right, top, botton = 0, n - 1, 0, n - 1
+        while num <= n*n:
+            for index in range(left, right + 1):
+                matrix[top][index] = num
+                num += 1
+            top += 1
+        
+            for index in range(top, botton + 1):
+                matrix[index][right] = num
+                num += 1
+            right -= 1
+        
+            for index in range(right, left-1, -1):
+                matrix[botton][index] = num
+                num += 1
+            botton -= 1
+        
+            for index in range(botton, top-1, -1):
+                matrix[index][left] = num
+                num += 1
+            left += 1
+    
+        return matrix
--- test/main.py
+++ test/main.py
@@ -1,14 +1,32 @@
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-def transpose(matrix):
-    rows = len(matrix)
-    columbs = len(matrix[0])
+n = 3
+def generateMatrix(n):
+    matrix = [[0] * n for i in range(n)]
+    num = 1
+    left, right, top, botton = 0, n - 1, 0, n - 1
+    while num <= n*n:
+        for index in range(left, right + 1):
+            matrix[top][index] = num
+            num += 1
+        top += 1
+        
+        for index in range(top, botton + 1):
+            matrix[index][right] = num
+            num += 1
+        right -= 1
+        
+        for index in range(right, left-1, -1):
+            matrix[botton][index] = num
+            num += 1
+        botton -= 1
+        
+        for index in range(botton, top-1, -1):
+            matrix[index][left] = num
+            num += 1
+        left += 1
     
-    transpose_matrix = [[0] * rows for i in range(columbs)]
+    return matrix
     
-    for i in range(rows):
-        for j in range(columbs):
-            transpose_matrix[j][i] = matrix[i][j] 
-            
-    return transpose_matrix
+gn = generateMatrix(5)
 
-print(transpose(matrix))
+for row in gn:
+    print(row)       
