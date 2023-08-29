da20420301b5cc8fde817919e03468a147194db6
tima <tima.j.zh@gmail.com>
2023-08-29 18:53:44 +0600

    search in rotated sorted array

---
+++ medium/33.py
@@ -0,0 +1,6 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        for index in range(len(nums)):
+            if nums[index] == target:
+                return index
+        return -1
--- test/main.py
+++ test/main.py
@@ -1,16 +1,14 @@
-matrix = [[3,-7,8],[9,-11,13],[15,-16,17]]
+matrix = [3, -7, 8, 6, 2]
 
-def countNegative(self, grid):
-    row, columb = len(grid), len(grid[0])
-    count = 0
-    for index_i in range(row):
-        for index_j in range(columb):
-            if grid[index_i][index_j] < 0:
-                count+=1
-                
-    return count
-s = 0
-print(countNegative(s, matrix))
+def search(nums, target):
+    
+    for index in range(len(nums)):
+        if nums[index] == target:
+            return index
+    return -1
+    
+print(search(matrix, target=8))
+    
             
     
     
